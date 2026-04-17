06d2f891292f55a9d185fc67b095b2bf46d280d3
Jeffer512 <140467995+Jeffer512@users.noreply.github.com>
2026-04-16 22:38:25 -0500

    feat: add routing and context provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import ResultsPage from './pages/ResultsPage';
+import Details from './pages/Details';
+
 function App() {
   return (
-    <div>
-      <h1>React News App</h1>
-    </div>
-  )
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/search/:keyword" element={<ResultsPage />} />
+      <Route path="/article/:uri" element={<Details />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
