85116964b943a73e4c9b615f0ad349fd9dfc74d4
Jeffer512 <140467995+Jeffer512@users.noreply.github.com>
2026-04-20 15:11:26 -0500

    chore: add GitHub Actions deployment workflow

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import './index.css';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <NewsProvider>
-      <BrowserRouter>
+      <BrowserRouter basename='react-news-app'>
         <App />
       </BrowserRouter>
     </NewsProvider>
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://jeffer512.github.io/react-news-app',
 })
+  
